2e6c6e9a3e5d50fa3946326322abffda8e30fc4c
Fachrul CHoliludddin <fachrul.fch@gmail.com>
2015-04-29 01:57:55 +0700

    conversi waktu ke asia/jakarta

--- _include/chat.php
+++ _include/chat.php
@@ -4,13 +4,15 @@ require_once 'db_function.php';
 //karena chat.php akan menggunakan fungsi dari sana
 
 function Chat_save_tanya($chat_trip_id,$chat_sender,$chat_mesej){
+        date_default_timezone_set('asia/jakarta');
+        $waktu          = date("Y-m-d H:i:s", time());
 	// sanitize inputan
 	$chat_trip_id 	= (int) $chat_trip_id;
 	$chat_sender 	= sanitize($chat_sender);
 	$chat_mesej 	= potong(sanitize($chat_mesej),250);
 	
-	$sql = "INSERT INTO tb_chat(chat_trip_id, chat_sender, chat_type, chat_mesej)
-		VALUES ('{$chat_trip_id}', '{$chat_sender}', '2', '{$chat_mesej}')";
+	$sql = "INSERT INTO tb_chat(chat_trip_id, chat_sender, chat_type, chat_mesej, chat_date)
+		VALUES ('{$chat_trip_id}', '{$chat_sender}', '2', '{$chat_mesej}','{$waktu}')";
 	$sqlInsert = good_query($sql);
 	
 	if ($sqlInsert){
